dae73c37bc7d04339b4b4a335e496f83c2ed5651
bounmykone <bounmyboundathip@gmail.com>
2025-05-12 09:22:29 +0700

    first commit VDO 01

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+
+    @vite('resources/css/app.css')
+    
+</head>
+<body>
+    <h1>Laravel App</h1>
+    
+    <hr>
+
+    <div id="vue-app"></div>
+
+    @vite('resources/js/app.js')
+
+</body>
+</html>
